388b810928b595df2d245ad6692729f9fc5259c9
Rolex Omondi <rolexomondi46@gmail.com>
2023-09-15 12:58:49 +0300

    final commit

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
@@ -12,6 +13,30 @@ class Person(db.Model,SerializerMixin):
     age = db.Column(db.Integer)
     origin = db.Column(db.String)
     email = db.Column(db.String)
+
+    @validates('name')
+    def validate_name(self,key,name):
+        if not isinstance(name, str):
+            raise ValueError('Name must be a string')
+        return name
+    
+    @validates('age')
+    def validate_age(self,key,age):
+        if not isinstance(age,int):
+            raise ValueError('age must be a number')
+        return age
+    
+    @validates('origin')
+    def validate_name(self,key,origin):
+        if not isinstance(origin, str):
+            raise ValueError('origin must be a string')
+        return origin
+         
+    @validates('email')
+    def validate_email(self, key, address):
+        if '@' not in address:
+            raise ValueError("failed simple email validation")
+        return address        
 
     
     def __repr__(self):
